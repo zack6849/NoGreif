23d2d1e57a646baaa94225ab0deb5dc7785d2b50
Zack Craig <zcraig29@yahoo.com>
2012-09-13 03:03:08 -0400

    Y u no commit

--- src/com/gmail/zcraig29/BlockPlace.java
+++ src/com/gmail/zcraig29/BlockPlace.java
@@ -16,7 +16,7 @@ import org.bukkit.event.player.PlayerBucketFillEvent;
 import org.bukkit.plugin.Plugin;
 
 public class BlockPlace implements Listener {
-	String prefix = ChatColor.YELLOW + "[" + ChatColor.GOLD + "NoGrief" + ChatColor.YELLOW + "] " + ChatColor.GREEN;
+	String prefix = ChatColor.YELLOW + "[" + ChatColor.GOLD + "griefshield" + ChatColor.YELLOW + "] " + ChatColor.GREEN;
 	Plugin plugin;
 	public BlockPlace(noGrief noGrief) {
 		plugin = noGrief;
@@ -32,10 +32,10 @@ public class BlockPlace implements Listener {
 		String bl = b.getTypeId() + ":" + b.getData();
 		List<String> nope = plugin.getConfig().getStringList("blacklist");
 		if(nope.contains(bl)){
-			if(!p.hasPermission("nogrief.blacklist.bypass")){
+			if(!p.hasPermission("griefshield.blacklist.bypass")){
 				event.setCancelled(true);
 				p.sendMessage(ChatColor.RED + "You arent allowed to place this block!");
-				Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to place block " + bl + "(" + b2 + ")" + " at " + location + " in world : " + p.getWorld().getName(), "nogrief.alert");
+				Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to place block " + bl + " ( " + b2 + " )" + " at " + location + " in world : " + p.getWorld().getName(), "griefshield.alert");
 				if(kick){
 					p.kickPlayer(kickmsg);
 				}
@@ -53,9 +53,9 @@ public class BlockPlace implements Listener {
 		Boolean kick = plugin.getConfig().getBoolean("kick");
 		if(bn.getType().equals(Material.LAVA) || bn.getType().equals(Material.STATIONARY_LAVA)){
 			if(plugin.getConfig().getBoolean("lava-fill-blocked")){
-				if(!event.getPlayer().hasPermission("nogrief.bucket.lava.fill")){
+				if(!event.getPlayer().hasPermission("griefshield.bucket.lava.fill")){
 					event.setCancelled(true);
-					Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to get lava with a bucket at " + location + " in world : " + p.getWorld().getName(), "nogrief.alert");
+					Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to get lava with a bucket at " + location + " in world : " + p.getWorld().getName(), "griefshield.alert");
 					p.sendMessage(prefix + message);
 					if(kick){
 						p.kickPlayer(kickmsg);
@@ -74,9 +74,9 @@ public class BlockPlace implements Listener {
 		Boolean kick= plugin.getConfig().getBoolean("kick");
 		if(bn.getType().equals(Material.WATER) || bn.getType().equals(Material.STATIONARY_WATER)){
 			if(plugin.getConfig().getBoolean("water-fill-blocked")){
-				if(!event.getPlayer().hasPermission("nogrief.bucket.water.fill")){
+				if(!event.getPlayer().hasPermission("griefshield.bucket.water.fill")){
 					event.setCancelled(true);
-					Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to get water with a bucket at " + location + " in world : " + p.getWorld().getName(), "nogrief.alert");
+					Bukkit.broadcast(prefix + "player " + p.getName() + " Tried to get water with a bucket at " + location + " in world : " + p.getWorld().getName(), "griefshield.alert");
 					p.sendMessage(prefix + message);
 					if(kick){
 						p.kickPlayer(kickmsg);
@@ -89,14 +89,16 @@ public class BlockPlace implements Listener {
 	public void BucketEmptyLava(PlayerBucketEmptyEvent event){
 		Block b = event.getBlockClicked();
 		String p = event.getPlayer().getName();
-		String location = b.getX() + "," + b.getY() + "," + b.getZ();
 		String kickmsg = plugin.getConfig().getString("kick-message");
-		Boolean kick= plugin.getConfig().getBoolean("kick");
+		Boolean kick = plugin.getConfig().getBoolean("kick");
+		String location = b.getX() + "," + b.getY() + "," + b.getZ();
 		if(event.getBucket().equals(Material.LAVA_BUCKET)){
+			Bukkit.broadcastMessage("debug");
+			System.out.println(event.getBucket()); 
 			if(plugin.getConfig().getBoolean("lava-blocked")){
-				if(!event.getPlayer().hasPermission("nogrief.bucket.lava.use")){
+				if(!event.getPlayer().hasPermission("griefshield.bucket.lava.use")){
 					event.setCancelled(true);
-					Bukkit.broadcast(prefix + "player " + p + " Tried to use a bucket of lava at " + location + " in world " + event.getPlayer().getWorld().getName(), "nogrief.alert");
+					Bukkit.broadcast(prefix + "player " + p + " Tried to use a bucket of lava at " + location + " in world " + event.getPlayer().getWorld().getName(), "griefshield.alert");
 					if(kick){
 						event.getPlayer().kickPlayer(kickmsg);
 					}
@@ -112,10 +114,12 @@ public class BlockPlace implements Listener {
 		Boolean kick = plugin.getConfig().getBoolean("kick");
 		String location = b.getX() + "," + b.getY() + "," + b.getZ();
 		if(event.getBucket().equals(Material.WATER_BUCKET)){
+			System.out.println(event.getBucket()); 
+			Bukkit.broadcastMessage("debug");
 			if(plugin.getConfig().getBoolean("water-blocked")){
-				if(!event.getPlayer().hasPermission("nogrief.bucket.water.use")){
+				if(!event.getPlayer().hasPermission("griefshield.bucket.water.use")){
 					event.setCancelled(true);
-					Bukkit.broadcast(prefix + "player " + p + " Tried to use a bucket of water at " + location + " in world " + event.getPlayer().getWorld().getName(), "nogrief.alert");
+					Bukkit.broadcast(prefix + "player " + p + " Tried to use a bucket of water at " + location + " in world " + event.getPlayer().getWorld().getName(), "griefshield.alert");
 					if(kick){
 						event.getPlayer().kickPlayer(kickmsg);
 					}
--- src/com/gmail/zcraig29/noGrief.java
+++ src/com/gmail/zcraig29/noGrief.java
@@ -32,31 +32,69 @@ public class noGrief extends JavaPlugin {
 
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-		String prefix = ChatColor.YELLOW + "[" + ChatColor.GOLD + "NoGrief" + ChatColor.YELLOW + "] " + ChatColor.GREEN;
-		if(cmd.getName().equalsIgnoreCase("nogrief") && (args.length >= 1)){
+		String prefix = ChatColor.YELLOW + "[" + ChatColor.GOLD + "GriefShield" + ChatColor.YELLOW + "] " + ChatColor.GREEN;
+		if(cmd.getName().equalsIgnoreCase("griefshield") && (args.length >= 1)){
 				if(args[0].equalsIgnoreCase("reload")){
-					if(sender.hasPermission("nogrief.reload")){
+					if(sender.hasPermission("griefshield.reload")){
 						this.reloadConfig();
 						sender.sendMessage(prefix  + "Configuration has been reloaded.");	
 						return true;	
 					}
-					if(!sender.hasPermission("nogrief.reload")){
+					if(!sender.hasPermission("griefshield.reload")){
 						sender.sendMessage(prefix + ChatColor.RED + "Error: You don't have permission to do this!");
 					}
 				}
 				if(args[0].equalsIgnoreCase("info")){
-					if(sender.hasPermission("nogrief.info")){
+					if(sender.hasPermission("griefshield.info")){
 						String version = this.getDescription().getVersion();
-						sender.sendMessage(ChatColor.GOLD + "========" + ChatColor.YELLOW + "NoGrief" + ChatColor.GOLD + "========");
-						sender.sendMessage(ChatColor.YELLOW + "Current Version: " + version);
-						sender.sendMessage(ChatColor.YELLOW + "Author: " + getDescription().getAuthors());
+						sender.sendMessage(ChatColor.GOLD + "========" + ChatColor.YELLOW + "Griefshield" + ChatColor.GOLD + "========");
+						sender.sendMessage(ChatColor.YELLOW + "Version: " + version);
+						sender.sendMessage(ChatColor.YELLOW + "Author: zack6849");
 						sender.sendMessage(ChatColor.YELLOW + "Where to look for updates:");
 						sender.sendMessage(ChatColor.YELLOW + getDescription().getWebsite());
 						sender.sendMessage(ChatColor.GOLD + "======================");
 						return true;
 					}
-					if(!sender.hasPermission("nogrief.info")){
+					if(!sender.hasPermission("griefshield.info")){
 						sender.sendMessage(prefix + ChatColor.RED + "Error: you don't have permission to do this!");
+					}
+				}
+				if(args[0].equalsIgnoreCase("status")){
+					if(sender.hasPermission("griefshield.status")){
+						sender.sendMessage(ChatColor.GOLD + "========" + ChatColor.YELLOW + "GriefShield" + ChatColor.GOLD + "========");
+						if(getConfig().getBoolean("water-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Water protection: " + ChatColor.GREEN+ "Enabled");
+						}
+						if(!getConfig().getBoolean("water-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Water protection: " + ChatColor.RED+ "Disabled");
+						}
+						if(getConfig().getBoolean("water-fill-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Water bucket protection: " + ChatColor.GREEN+ "Enabled");
+						}
+						if(!getConfig().getBoolean("water-fill-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Water bucket protection: " + ChatColor.RED+ "Disabled");
+						}
+						if(getConfig().getBoolean("lava-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Lava protection: " + ChatColor.GREEN+ "Enabled");
+						}
+						if(!getConfig().getBoolean("lava-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Lava protection: " + ChatColor.RED+ "Disabled");
+						}
+						if(getConfig().getBoolean("lava-fill-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Lava bucket protection: " + ChatColor.GREEN+ "Enabled");
+						}
+						if(!getConfig().getBoolean("lava-fill-blocked")){
+							sender.sendMessage(ChatColor.YELLOW + "Lava bucket protection " + ChatColor.RED+ "Disabled");
+						}
+						if(getConfig().getBoolean("kick")){
+							sender.sendMessage(ChatColor.YELLOW + "Kick: " + ChatColor.GREEN + "Enabled");
+						}
+						if(!getConfig().getBoolean("kick")){
+							sender.sendMessage(ChatColor.YELLOW + "Kick: " + ChatColor.RED + "Disabled");
+						}
+						return true;
+					}else{
+						sender.sendMessage(ChatColor.RED + "Error: you don't have permission to do this!");
 					}
 				}
 			}
